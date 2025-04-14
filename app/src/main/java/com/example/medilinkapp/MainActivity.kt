5fa802b464a36f5f01e663fe3cc8df9a321b5834
Anne Apiyo <anneapiyo0@gmail.com>
2025-04-14 22:16:21 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/medilinkapp/MainActivity.kt
+++ app/src/main/java/com/example/medilinkapp/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import com.example.medilinkapp.ui.navigation.AppNavGraph
 import com.example.medilinkapp.ui.theme.MedilinkAppTheme
 
-class MainActivity : ComponentActivity() {
+ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         // Switch to the normal theme before setting the content
         setTheme(R.style.Theme_MedilinkApp)
